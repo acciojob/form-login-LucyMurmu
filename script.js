56a45c53eb513e8e61b9f5d778ef48510a0ac578
LucyMurmu <btech10543.22@bitmesra.ac.in>
2026-06-02 05:27:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	event.preventDefault();
+	let fname=document.getElementsByName("fname")[0].value.trim();
+	let lname=document.getElementsByName("lname")[0].value.trim();
+	alert(fname+" "+lname);
 
 }
